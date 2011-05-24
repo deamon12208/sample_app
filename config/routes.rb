a03052c7f571a8dec4c0268fa003a036393f24a4
David Petty <deamon12208@me.com>
2011-05-23 22:18:04 -0400

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ SampleApp::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
+  root :to => 'pages#home'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
