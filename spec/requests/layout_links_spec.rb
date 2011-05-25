aa54da95871a48db4091f2588df85e142d5815e2
David Petty <deamon12208@me.com>
2011-05-25 01:23:40 -0400

    finished Layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -20,5 +20,10 @@ describe "LayoutLinks" do
   it "should have a Help page at '/help'" do
     get '/help'
     response.should have_selector('title', :content => "Help")
-  end  
+  end
+  
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end    
 end
